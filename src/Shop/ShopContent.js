a08496c73fbca68b6774f38f306330b3b9225fe2
jay4170 <jay.hammond@live.com>
2022-06-12 23:14:06 +0300

    Shop page working with basic information

--- src/Shop/ShopContent.js
+++ src/Shop/ShopContent.js
@@ -4,14 +4,21 @@ export default function ShopContent(props) {
   const products = props.products;
 
   return (
-    <ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
       {products.map((product) => {
         console.log(product);
         return (
-          <li>
-            <h3>{product.title}</h3>
-          </li>
+          <div className="text-center ">
+            <li className="justify-center ">
+              <img
+                alt={product.title}
+                className="px-10"
+                src={product.image}
+              ></img>
 
+              <h3 className="px-10 m-1">{product.title}</h3>
+            </li>
+          </div>
         );
       })}
     </ul>
